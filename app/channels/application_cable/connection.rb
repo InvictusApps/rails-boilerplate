2162907ced96c286a6a0e8880b3abb38ba8db15b
Dylan Gleicher <dgleiche@me.com>
2022-04-21 11:08:14 -0400

    Added cable boilerplate

--- app/channels/application_cable/connection.rb
+++ app/channels/application_cable/connection.rb
@@ -1,4 +1,21 @@
 module ApplicationCable
   class Connection < ActionCable::Connection::Base
+    identified_by :current_user
+
+    def connect
+      self.current_user = authenticate!
+    end
+
+    protected
+
+    def authenticate!
+      user = test_user
+
+      user || reject_unauthorized_connection
+    end
+
+    def test_user
+      request.params['test_user']
+    end
   end
 end
